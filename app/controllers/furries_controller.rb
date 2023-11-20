a8a74c653b5d45d30a1860897b5d891cb2728229
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-11-20 17:10:21 +0100

    Furries show working, create almost working

--- app/controllers/furries_controller.rb
+++ app/controllers/furries_controller.rb
@@ -1,4 +1,6 @@
 class FurriesController < ApplicationController
+  before_action :set_furrie, only: %i[show destroy]
+
   def index
     @furries = Furrie.all
   end
@@ -6,6 +8,31 @@ class FurriesController < ApplicationController
   def show
   end
 
+  def new
+    @furrie = Furrie.new
+  end
+
   def create
+    @furrie = Furrie.new(furrie_params)
+    if @furrie.save
+      redirect_to furries_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @furrie.destroy
+    redirect_to furries_path
+  end
+
+  private
+
+  def set_furrie
+    @furrie = Furrie.find(params[:id])
+  end
+
+  def furrie_params
+    params.require(:furrie).permit(:name, :description, :price, :fursona, :availibility)
   end
 end
