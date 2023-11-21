d30ac393e0638da9ed9a4a38458094479546eed5
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-11-21 16:21:43 +0100

    Add flatpickr to booking and range column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_140246) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_150807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_140246) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "range"
     t.index ["furrie_id"], name: "index_bookings_on_furrie_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
