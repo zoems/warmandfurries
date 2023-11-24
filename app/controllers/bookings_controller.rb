f828271a6c0cc3c7e9005702a305337b0cbda79a
Iratxe Garrido <iratxe.ggarrido@gmail.com>
2023-11-24 12:57:54 +0100

    Show my booked furries

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,8 +4,22 @@ class BookingsController < ApplicationController
   def index
     @mybookings = current_user.bookings
     @myfurries = current_user.furries
+    # key_skill if @myfurries[0].key_skill.nil?
     @my_furries_bookings = current_user.bookings_as_owner
   end
+
+  # def key_skill
+  #   @myfurries.map do |furry|
+  #   furry.key_skill = "Cuddleable"
+  #     if furry.cuddleable > furry.energy && furry.cuddleable > furry.tough_love
+  #       furry.key_skill = "Cuddleable"
+  #     elsif furry.tough_love > furry.energy && furry.tough_love > furry.cuddleable
+  #       furry.key_skill = "Tough Love"
+  #     elsif furry.energy > furry.tough_love && furry.energy > furry.cuddleable
+  #       furry.key_skill = "High Energy"
+  #     end
+  #   end
+  # end
 
   def update
     @answer = params[:commit]
--- app/controllers/furries_controller.rb
+++ app/controllers/furries_controller.rb
@@ -3,29 +3,26 @@ class FurriesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @furries = Furry.all
-    @furries = Furry.where.not(user_id: current_user.id)
-
-
-    key_skill if @furries[0].key_skill.nil?
+    current_user.nil? ?  @furries = Furry.all : @furries = Furry.where.not(user_id: current_user.id)
+    # key_skill if @furries[0].key_skill.nil?
     if params[:search].present?
       @query = params[:search][:query]
       @furries = @furries.where("name ILIKE ?", "%#{@query}%")
     end
   end
 
-  def key_skill
-    @furries.each do |furry|
-    furry.key_skill = "Cuddleable"
-      if furry.cuddleable > furry.energy && furry.cuddleable > furry.tough_love
-        furry.key_skill = "Cuddleable"
-      elsif furry.tough_love > furry.energy && furry.tough_love > furry.cuddleable
-        furry.key_skill = "Tough Love"
-      elsif furry.energy > furry.tough_love && furry.energy > furry.cuddleable
-        furry.key_skill = "High Energy"
-      end
-    end
-  end
+  # def key_skill
+  #   @furries.map do |furry|
+  #   furry.key_skill = "Cuddleable"
+  #     if furry.cuddleable > furry.energy && furry.cuddleable > furry.tough_love
+  #       furry.key_skill = "Cuddleable"
+  #     elsif furry.tough_love > furry.energy && furry.tough_love > furry.cuddleable
+  #       furry.key_skill = "Tough Love"
+  #     elsif furry.energy > furry.tough_love && furry.energy > furry.cuddleable
+  #       furry.key_skill = "High Energy"
+  #     end
+  #   end
+  # end
 
   def map
     @furries = Furry.all
@@ -41,7 +38,7 @@ class FurriesController < ApplicationController
 
   def my_furries
     @furries = Furry.where(user_id: current_user.id)
-    key_skill if @furries[0].key_skill.nil?
+    key_skill if !@furries.nil? || @furries[0].key_skill.nil?
   end
 
   def show
